7f5efafad07169e6c4abbea982da6e8365479b0d
kimjunyoung <kimjy80@sesisoft.com>
2022-01-12 10:10:23 +0900

    while이 아닌 수학공식으로 해결 초/중 수학 난이도인데 한참 생각했다..

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,27 @@
 import java.io.BufferedReader;
-
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        try (BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); ) {
-            int up= Integer.parseInt(bf.readLine());//하루ㅈ이동
-            int down=Integer.parseInt(bf.readLine());//하루 다운
-            int total=Integer.parseInt(bf.readLine());//총높이
-            int start=0;
-            int time=0;
-            while(true){
-                //System.out.println(start);
-                if(start>=total){
-                    break;
-                }
-                time+=1;
-                start+=up;
-                if(start>=total){
-                    break;
-                }else{
-                    start-=down;
-                }
-            }
-            System.out.println(time);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+		int up = Integer.parseInt(st.nextToken());
+		int down = Integer.parseInt(st.nextToken());
+		int total = Integer.parseInt(st.nextToken());
+        int mok=(total-down)/(up-down);
+        int nam=(total-down)%(up-down);
+        if(nam!=0){
+            mok+=1;
+        }  
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 선언
+        bw.write(Integer.toString(mok));
+        bw.flush(); // write로 담은 내용 출력 후, 버퍼를 비움.
+        bw.close(); 
 
-        }
+        
     }
 }
