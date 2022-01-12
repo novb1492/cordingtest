442610dfdd26e71ebd42f122f4a65c3f9f39350c
kimjunyoung <kimjy80@sesisoft.com>
2022-01-12 18:24:17 +0900

    소수는 역시 하나 만들어 놓으면 돌려쓰기 좋다

--- src/Main.java
+++ src/Main.java
@@ -10,42 +10,58 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
-        int t=Integer.parseInt((bf.readLine()));
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-        List<Integer>arr=new ArrayList<>();
-       while(st.hasMoreTokens()){
-            //for(int i=1;i<=1000;i++){
-                //arr.add(i);
+        int start=Integer.parseInt((bf.readLine()));
+        int end=Integer.parseInt((bf.readLine()));
+        //StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        int t=0;
+        boolean first=true;
+        int min=0;
+        for(int ii=start;ii<=end;ii++){
+            boolean flag=false;
+            int half=ii/2;
+           // System.out.println("===============");
+           // System.out.println("ii"+ii);
+            if(ii==0||ii==1){
+                //System.out.println("1,0");
+                
+            }else{
+                if(division(half, ii)){
+                    //System.out.println("x소수발견: "+ii);
+                    flag=true;
+                    
+                }
+                if(!flag){
+                    //System.out.println("소수발견: "+ii);
+                    if(first){
+                        min=ii;
+                        first=false;
+                    }
+                    t+=ii;
+                }
+            }
+           
+            //System.out.println("t: "+t+"min: "+min);
+            //System.out.println("===============");
 
-            //}
-           arr.add(Integer.parseInt(st.nextToken()));
         }
-        int c=0;
-        for(int i:arr){
-         System.out.println("시작:"+i);
-         if(i==1||i==0){
-            c++;
+        if(first){
+            System.out.println(-1);
+        }else{
+            System.out.println(t);
+            System.out.println(min);
         }
-         else if(division(i/2,i)){
-                c++;
-            
-         }
-        }
-     
-
-        System.out.println(arr.size()-c);
     }
     public static boolean division(int half,int max) {
-        System.out.println("half: "+half);
+       // System.out.println("half: "+half);
         for(int i=2;i<=half;i++){
             if(max%i==0){
-                System.out.println("나눠졌음:"+i);
-                System.out.println("===============");
+               // System.out.println("나눠졌음:"+i);
+              
                 return true;
             }
         }
-        System.out.println("나눠지지않음");
-        System.out.println("===============");
+        //System.out.println("나눠지지않음");
+      
         return false;
     }
     
