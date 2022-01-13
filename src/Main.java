8f94fe2210379c7ced948ea4dd98569db033499f
kimjunyoung <kimjy80@sesisoft.com>
2022-01-13 10:23:26 +0900

    어려워보인다..

--- src/Main.java
+++ src/Main.java
@@ -12,32 +12,11 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
-        int t=Integer.parseInt((bf.readLine()));
-        List<int[]>lIntegers=new ArrayList<>();
-        for(int i=0;i<t;i++){//입력 수 만큼 사람정보 받기
-            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-            int[] arr = new int[2];
-            arr[0]=Integer.parseInt(st.nextToken());
-            arr[1]=Integer.parseInt(st.nextToken());
-            lIntegers.add(arr);
-        }
-        String ranks="";
-        for(int i=0;i<lIntegers.size();i++){//배열수 검사
-            int rank=1;
-            for(int ii=0;ii<lIntegers.size();ii++){
-                if(ii!=i){//본인과 본인의 정보라면 건너뛰게
-                    int[]a=lIntegers.get(i);
-                    int[]b=lIntegers.get(ii);
-                    //System.out.println("a1:"+a[0]+" a2:"+a[1]+" b1:"+b[0]+" b2:"+b[1]);
-                    if(a[0]<b[0]&&a[1]<b[1]){//둘다 작아야 랭크가 떨어짐
-                        //System.out.println("up");
-                        rank++;
-                    }
-                }
-            }
-            ranks+=Integer.toString(rank)+",";
-        }
-        System.out.println(ranks.replace(",", "\n"));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        int width=Integer.parseInt(st.nextToken());
+        int height=Integer.parseInt(st.nextToken());
+        
+   
     }
    
 }
