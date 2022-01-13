f5aaf1a31982ba3b55d3c472af56b45c9814faa5
kimjunyoung <kimjy80@sesisoft.com>
2022-01-13 10:02:30 +0900

    아고 이전문제들 주석을 썼네

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,33 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num=Integer.parseInt(br.readLine());
-        if(num<13){
-            System.out.println(0);
-            return;
-        }
+       for(int i=1;i<num;i++){//num보다 작을때
+          // System.out.println(i);
+           int n=i;
+           int sum=0;
+           int total=0;
+           while(true){//몫이 0이될때까지 나눔
+            int a=n%10;
+            sum+=a;
+            n/=10;
+           if(n==0){//0이라면 나온 수 +원래수 더하기
+            //System.out.println(i+":"+sum+"="+(i+sum));
+            total=i+sum;
+            break;
+           }
+           }
+           if(total==num){//더한값이 원래수랑 같다면 출력
+               System.out.println(i);
+               return;
+           }
+       }
+       System.out.println(0);
+       
         
         
     }
