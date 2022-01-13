ec197c463f068a04a3794f96c5c639b5bcd8e91c
kimjunyoung <kimjy80@sesisoft.com>
2022-01-13 10:20:37 +0900

    무난했다

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 
@@ -11,43 +13,31 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
         int t=Integer.parseInt((bf.readLine()));
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-        List<Integer>arr=new ArrayList<>();
-       while(st.hasMoreTokens()){
-            //for(int i=1;i<=1000;i++){
-                //arr.add(i);
-
-            //}
-           arr.add(Integer.parseInt(st.nextToken()));
+        List<int[]>lIntegers=new ArrayList<>();
+        for(int i=0;i<t;i++){//입력 수 만큼 사람정보 받기
+            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+            int[] arr = new int[2];
+            arr[0]=Integer.parseInt(st.nextToken());
+            arr[1]=Integer.parseInt(st.nextToken());
+            lIntegers.add(arr);
         }
-        int c=0;
-        for(int i:arr){
-         System.out.println("시작:"+i);
-         if(i==1||i==0){
-            c++;
-        }
-         else if(division(i/2,i)){
-                c++;
-            
-         }
-        }
-     
-
-        System.out.println(arr.size()-c);
-    }
-    public static boolean division(int half,int max) {
-        System.out.println("half: "+half);
-        for(int i=2;i<=half;i++){
-            if(max%i==0){
-                System.out.println("나눠졌음:"+i);
-                System.out.println("===============");
-                return true;
+        String ranks="";
+        for(int i=0;i<lIntegers.size();i++){//배열수 검사
+            int rank=1;
+            for(int ii=0;ii<lIntegers.size();ii++){
+                if(ii!=i){//본인과 본인의 정보라면 건너뛰게
+                    int[]a=lIntegers.get(i);
+                    int[]b=lIntegers.get(ii);
+                    //System.out.println("a1:"+a[0]+" a2:"+a[1]+" b1:"+b[0]+" b2:"+b[1]);
+                    if(a[0]<b[0]&&a[1]<b[1]){//둘다 작아야 랭크가 떨어짐
+                        //System.out.println("up");
+                        rank++;
+                    }
+                }
             }
+            ranks+=Integer.toString(rank)+",";
         }
-        System.out.println("나눠지지않음");
-        System.out.println("===============");
-        return false;
+        System.out.println(ranks.replace(",", "\n"));
     }
-    
-
+   
 }
