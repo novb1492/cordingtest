52e9f4ffb3f633d03747e3ea2ff4ab96252f25d6
novb1492 <novb1492@naver.com>
2022-01-19 22:33:41 +0900

    문자열 정렬중

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,56 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
+import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.Map.Entry;
 
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
+        //            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
         int total=Integer.parseInt(bf.readLine());
-        int[][] arr =new int[total][2];
+        List<String>arr=new ArrayList<>();
         for(int i=0;i<total;i++){
-            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-            arr[i][0]=Integer.parseInt(st.nextToken());
-            arr[i][1]=Integer.parseInt(st.nextToken());
+            arr.add(bf.readLine());
         }
-        Arrays.sort(arr,(o1, o2) -> { 
-            if(o1[0] == o2[0]){ 
-                return Integer.compare(o1[1],o2[1]);
-             } else{ 
-                 return Integer.compare(o1[0],o2[0]); 
-             }
-        });
-        for(int[] a:arr){
-            System.out.println(a[0]+" "+a[1]);
+        Map<Integer,String>map=new HashMap<>();
+        int max=0;
+        for(String s:arr){
+            int rank=0;
+            System.out.println("s: "+s);
+            for(String ss:arr){
+                if(!s.equals(ss)){
+                    System.out.println("s: "+s+" ss: "+ss);
+                    if(s.length()>ss.length()){
+                        System.out.println("김");
+                        rank++;
+                    }else if(s.length()==ss.length()){
+                        char a=s.charAt(0);
+                        char b=s.charAt(0);
+                        if((int)a>(int)b){
+                            System.out.println("뒤에 짤");
+                            rank++;
+                        }
+                    }
+                }
+            }
+            if(max<rank){
+                max=rank;
+            }
+            System.out.println("s: "+s+" rank: "+rank);
+            map.put(rank, s);
+        }
+        System.out.println("-----------------");
+        for(int i=0;i<max;i++){
+            System.out.println(map.get(i));
         }
 
        
