3875fc2bb586290023d450c220e16bf0b7e88819
kimjunyoung <kimjy80@sesisoft.com>
2022-01-24 15:05:26 +0900

    시간초과 계속나네

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,45 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 
 public class Main {
-
+    
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
+        // StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
         int total=Integer.parseInt(bf.readLine());
-        int[][] arr =new int[total][2];
+        int[]arr=new int[total+1];
         for(int i=0;i<total;i++){
-            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-            arr[i][0]=Integer.parseInt(st.nextToken());
-            arr[i][1]=Integer.parseInt(st.nextToken());
+            int in=Integer.parseInt(bf.readLine());
+            int a=arr[in];
+            a+=1;
+            arr[in]=a;
+            //System.out.println("in: "+arr[in]);
         }
-        Arrays.sort(arr,(o1, o2) -> { 
-            if(o1[0] == o2[0]){ 
-                return Integer.compare(o1[1],o2[1]);
-             } else{ 
-                 return Integer.compare(o1[0],o2[0]); 
-             }
-        });
-        for(int[] a:arr){
-            System.out.println(a[0]+" "+a[1]);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+        int s=arr.length;
+        for(int i=0;i<s;i++){
+            int a=arr[i];
+             if(a>0){
+                 for(int ii=0;ii<a;ii++){
+                    bw.write(i+"\n");   //버퍼에 있는 값 전부 출력
+                    bw.flush();   //남아있는 데이터를 모두 출력시킴 
+                 }
+            }
         }
-
+        bw.close();
        
       
      
