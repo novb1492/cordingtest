ed88794408462c4f542ae7eaee4d5b91346c01d4
kimjunyoung <kimjy80@sesisoft.com>
2022-01-24 14:38:01 +0900

    시간초과나서 이전에 썼던 방법으로 통과

--- src/Main.java
+++ src/Main.java
@@ -19,33 +19,23 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
         int total=Integer.parseInt(bf.readLine());
-        List<String> arr = new ArrayList<>();
+        String[] arr = new String[total];
         for(int i=0;i<total;i++){
             StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-            arr.add(st.nextToken()+","+st.nextToken());
+            arr[i]=st.nextToken()+" "+st.nextToken();
         }
-        int size=arr.size();
-        String[]arr2=new String[size];
-        for(int i=0;i<size;i++){
-            int rank=0;
-            String s=arr.get(i);
-            for(int ii=0;ii<size;ii++){
-                int age=Integer.parseInt(s.split(",")[0]);
-                int age2=Integer.parseInt(arr.get(ii).split(",")[0]);
-                if(age>age2){
-                    rank++;
-                }else if(age==age2){
-                    if(i>ii){
-                        rank++;
-                    }
-                }
+        Arrays.sort(arr,(o1, o2) -> { 
+            int a=Integer.parseInt(o1.split(" ")[0]);
+            int b=Integer.parseInt(o2.split(" ")[0]);
+            if(a==b){
+                return 0;
             }
-            arr2[rank]=s;
-        }
+            return Integer.compare(a, b);
+        });
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
-        for(String s:arr2){
-                  bw.write(s.replace(",", " ")+"\n");   //버퍼에 있는 값 전부 출력
-        bw.flush();   //남아있는 데이터를 모두 출력시킴
+        for(String s:arr){
+            bw.write(s+"\n");   //버퍼에 있는 값 전부 출력
+            bw.flush();   //남아있는 데이터를 모두 출력시킴
 
         }
         bw.close();
