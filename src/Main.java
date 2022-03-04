083ef38650df3e6b42ffb28520ea8458140b5627
kimjunyoung <kimjy80@sesisoft.com>
2022-03-04 18:30:27 +0900

    주말동안 dfs 만 공부해보자

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,11 @@ import java.util.stream.Collectors;
 
 public class Main {
     
+    static boolean[] vistied = new boolean[9];
+    // 그림예시 그래프의 연결상태를 2차원 배열로 표현
+    // 인덱스가 각각의 노드번호가 될 수 있게 0번인덱스는 아무것도 없는 상태라고 생각하시면됩니다.
+    static int[][] graph = {{}, {2,3,8}, {1,6,8}, {1,5}, {5,7}, {3,4,7}, {2}, {4,5}, {1,2}};
+
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
         /*StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
@@ -24,7 +29,9 @@ public class Main {
         int check=0;
         Map<String,Boolean>distinctByKey=new HashMap<>();
         List<String>arr=new ArrayList<>();*/
-        dfs(10);
+        dfs(1);
+        int[][][] a=new int[10][10][10];
+
         //arr = arr.stream().distinct().collect(Collectors.toList());
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
@@ -43,12 +50,19 @@ public class Main {
    
     }
     private static void dfs(int n) {
-        if(n==0){
-            return;
-        }else{
-            dfs(n-1);
-            System.out.println(n);
+       // 방문 처리
+        vistied[n] = true;
+        // 방문 노드 출력
+        System.out.print(n + " , ");
+        System.out.print(Arrays.toString(graph[n]) + " -> ");
+        // 방문한 노드에 인접한 노드 찾기
+        for (int node : graph[n]) {
+            // 인접한 노드가 방문한 적이 없다면 DFS 수행
+            if(!vistied[node]) {
+                dfs(node);
+            }
         }
+
     }
    
 }
