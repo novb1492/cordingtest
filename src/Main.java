165a1945671d2d9b5fa846a51853c9b5a00af37c
novb1492 <novb1492@naver.com>
2022-03-06 21:21:03 +0900

    원리

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,28 @@ public class Main {
     // 그림예시 그래프의 연결상태를 2차원 배열로 표현
     // 인덱스가 각각의 노드번호가 될 수 있게 0번인덱스는 아무것도 없는 상태라고 생각하시면됩니다.
     //static int[][] graph = {{}, {1,2,3,4}, {1,2,3,4}, {1,2,3,4}, {1,2,3,4}};
-    //1* start --4
+    //1* start --4 before =1
     //1.1,1.2*,before=2,2.1,2.2,2.3*,before=3,3.1,3.2,3.3,3.4*,before=4,4.1,4.2,4.3,4.4=1234 
     //4false 
     //3false 2.4* before=4, 4.1,4.2,4.3* return;
-    
+    //3false
+    //4false
+    //1.3* before=3,3.1,3.2* return;
+    //2false
+    //1.4* before=4,4.1,4.2* return;
+    //2false
+    //4false
+    //----------------------------
+    //2 * start --3 before=2
+    //2.1* return;
+    //1 false
+    //2.3* before=3,3.1* return;
+    //1false 3.2,3.4* =234
+    //4false
+    //3false 2.4*,4.1* return;
+    //1false
+    //4 false
+    //------------------------------
     
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
@@ -54,7 +71,7 @@ public class Main {
         vistied[n] = true;
         if(beforeN!=0){
             if(beforeN>n){
-                System.out.println(beforeN+":"+n);
+               // System.out.println(beforeN+":"+n);
                 return;
             }
         }
