b593cad40ac211dc51a703f858cba3bed955c226
novb1492 <novb1492@naver.com>
2022-03-06 12:14:50 +0900

    슬슬 이해 가는중

--- src/Main.java
+++ src/Main.java
@@ -16,10 +16,10 @@ import java.util.stream.Collectors;
 
 public class Main {
     
-    static boolean[] vistied = new boolean[9];
+    static boolean[] vistied = new boolean[5];
     // 그림예시 그래프의 연결상태를 2차원 배열로 표현
     // 인덱스가 각각의 노드번호가 될 수 있게 0번인덱스는 아무것도 없는 상태라고 생각하시면됩니다.
-    static int[][] graph = {{}, {2,3,8}, {1,6,8}, {1,5}, {5,7}, {3,4,7}, {2}, {4,5}, {1,2}};
+    static int[][] graph = {{}, {1,2,3}, {2,1}, {3,4}, {3,4}};
 
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
@@ -54,7 +54,6 @@ public class Main {
         vistied[n] = true;
         // 방문 노드 출력
         System.out.print(n + " , ");
-        System.out.print(Arrays.toString(graph[n]) + " -> ");
         // 방문한 노드에 인접한 노드 찾기
         for (int node : graph[n]) {
             // 인접한 노드가 방문한 적이 없다면 DFS 수행
