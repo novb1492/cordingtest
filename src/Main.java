1d6e988d9a036d58d7579a356d8e3ffca3f744df
kimjunyoung <kimjy80@sesisoft.com>
2022-03-08 14:34:46 +0900

    종료 시간만 비교하면될듯

--- src/Main.java
+++ src/Main.java
@@ -16,53 +16,42 @@ import java.util.stream.Collectors;
 
 public class Main {
     
-    static boolean[] vistied = new boolean[9];
-    // 그림예시 그래프의 연결상태를 2차원 배열로 표현
-    // 인덱스가 각각의 노드번호가 될 수 있게 0번인덱스는 아무것도 없는 상태라고 생각하시면됩니다.
-    static int[][] graph = {{}, {2,3,8}, {1,6,8}, {1,5}, {5,7}, {3,4,7}, {2}, {4,5}, {1,2}};
-
+  
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); 
-        /*StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-        int max=Integer.parseInt(st.nextToken());
-        int num=Integer.parseInt(st.nextToken());
-        int check=0;
-        Map<String,Boolean>distinctByKey=new HashMap<>();
-        List<String>arr=new ArrayList<>();*/
-        dfs(1);
-        int[][][] a=new int[10][10][10];
-
-        //arr = arr.stream().distinct().collect(Collectors.toList());
+        int totalCount=Integer.parseInt(bf.readLine());
+        List<int[]>times=new ArrayList<>();
+        for(int i=0;i<totalCount;i++){
+            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+            int[] startAndEnd=new int[2];
+            startAndEnd[0]=Integer.parseInt(st.nextToken());
+            startAndEnd[1]=Integer.parseInt(st.nextToken());
+            times.add(startAndEnd);
+        }
+        List<int[]>sortArr=new ArrayList<>();
+        for(int i=0;i<times.size();i++){
+            int endTime=times.get(i)[1];
+            System.out.println(endTime);
+            for(int ii=0;ii<times.size();ii++){
+                if(i!=ii){
+                    if(endTime>=times.get(ii)[0]){
+                       // System.out.println("start:"+times.get(i)[0] );
+                        int[] startAndEnd=new int[2];
+                        startAndEnd=times.get(ii);
+                        sortArr.add(startAndEnd);
+                        endTime=times.get(ii)[0];
+                    }
+                }
+            }
+        }
+        for(int[]a:sortArr){
+            System.out.println(Arrays.toString(a));
+        }
 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
-        /*int s=arr.length;
-        for(int i=0;i<s;i++){
-            int a=arr[i];
-             if(a>0){
-                 for(int ii=0;ii<a;ii++){
-                    bw.write(i+"\n");   //버퍼에 있는 값 전부 출력
-                    bw.flush();   //남아있는 데이터를 모두 출력시킴 
-                 }
-            }
-        }
-        bw.close();*/
-
+  
    
     }
-    private static void dfs(int n) {
-       // 방문 처리
-        vistied[n] = true;
-        // 방문 노드 출력
-        System.out.print(n + " , ");
-        System.out.print(Arrays.toString(graph[n]) + " -> ");
-        // 방문한 노드에 인접한 노드 찾기
-        for (int node : graph[n]) {
-            // 인접한 노드가 방문한 적이 없다면 DFS 수행
-            if(!vistied[node]) {
-                dfs(node);
-            }
-        }
 
-    }
    
 }
