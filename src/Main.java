74127cf61c8b45a3908fbcd4c8c678a9f75ecbaf
kimjunyoung <kimjy80@sesisoft.com>
2022-03-11 17:43:46 +0900

    아니 이거 출력 순서 어캐 빼야하는거

--- src/Main.java
+++ src/Main.java
@@ -21,12 +21,12 @@ public class Main {
     
   
     public static void main(String[] args) throws Exception {
-        int[] fees={120, 0, 60, 591};
+        int[] fees={180, 5000, 10, 600};
         /*fees[0]=180;//기본시간
         fees[1]=5000;//기본요금  
         fees[2]=10;//n분당
         fees[3]=600;//요금*/
-        String[] records={"16:00 3961 IN","16:00 0202 IN","18:00 3961 OUT","18:00 0202 OUT","23:58 3961 IN"};
+        String[] records={"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
 
         
         System.out.println(Arrays.toString(solution(fees, records)));
