33684f02832f90082ca1db56971202335e668ec3
kimjunyoung <kimjy80@sesisoft.com>
2022-03-11 15:55:37 +0900

    셋팅

--- src/Main.java
+++ src/Main.java
@@ -21,15 +21,29 @@ public class Main {
   
     public static void main(String[] args) throws Exception {
         int[] fees=new int[4];
-        String[] records=new String[4];
-        
+        fees[0]=180;//기본시간
+        fees[1]=5000;//기본요금  
+        fees[2]=10;//n분당
+        fees[3]=600;//요금
+        String[] records={"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
 
         
-        System.out.println(Arrays.toString(solution(id_list, report, k2)));
+        System.out.println(Arrays.toString(solution(fees, records)));
            
         
     }
-    public static solution(int[] fees, String[] records) {
+    public static int[] solution(int[] fees, String[] records) {
+        Map<String,String>cars=new HashMap<>();
+        for(String record:records){
+            String carNum=record.split(" ")[1];
+            if(cars.containsKey(carNum)){
+                String infors=cars.get(carNum);
+                infors=infors+","+record;
+            }else{
+                cars.put(carNum, record);
+            }
+        }
+        System.out.println(cars.toString());
         int[] answer = {};
         return answer;
     }
